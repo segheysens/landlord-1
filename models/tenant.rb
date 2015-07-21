74857b78b7028e1b4a22f804c01f354cfd11cb09
Stephen Gheysens <segheysens@gmail.com>
2015-07-21 07:27:32 -0400

    Added code for Apartment and Tenant models, verfified all tests were passing

---
+++ models/tenant.rb
@@ -0,0 +1,13 @@
+class Tenant
+
+  attr_reader :name
+  attr_reader :age
+  attr_reader :gender
+  
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+end
